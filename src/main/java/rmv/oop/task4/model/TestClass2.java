99e8949c5d382a617bd63b7c98afe2f3e64324cb
Rostyslav M <mrostyk55@gmail.com>
2020-09-20 22:38:10 +0300

    finished with some tests

--- src/main/java/rmv/oop/task4/model/TestClass2.java
+++ src/main/java/rmv/oop/task4/model/TestClass2.java
@@ -1,4 +1,18 @@
 package rmv.oop.task4.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class TestClass2 {
+    private TestClass1 testClass1;
+    private Boolean aBoolean;
+
+    private int get100(){
+        return 100;
+    }
+
 }
